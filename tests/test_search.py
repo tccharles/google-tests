e5a506fe1dac80e861d1c6fd3e59e8b9a61fe035
Teri Charles§q! <tericharles@juno.com>
2015-03-22 16:27:53 -0600

    take out old code

--- tests/test_search.py
+++ tests/test_search.py
@@ -11,18 +11,13 @@ from pages.profile import Profile
 
 class TestSearch:
 
-	#@pytest.mark.nondestructive
+    #@pytest.mark.nondestructive
     @pytest.mark.xfail("config.getvalue('base_url') == 'https://www.google.com'")
-	def test_that_search_returns_relevant_results(self, Google):
-		query = u'Boulder'
-		home_page = Home(Google)
-		search_page = home_page.header.search_for(query)
-		Assert.true(search_page.results_count > 0)
+    def test_that_search_returns_relevant_results(self, Google):
+	    query = u'Boulder'
+	    home_page = Home(Google)
+	    search_page = home_page.header.search_for(query)
+	    Assert.true(search_page.results_count > 0)
 
-		element_search = browser.find_element_by_id('lst-ib') # id of search field
+	    element_search = browser.find_element_by_id('lst-ib') # id of search field
 			#element_search.send_keys('Boulder') # input 'Boulder' into search field
-		search_term = 'Boulder'
-		element_search.send_keys(search_term)
-		element_search.send_keys(Keys.RETURN) # hits return after you enter search text
-		
-		browser.close()
