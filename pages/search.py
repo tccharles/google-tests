61609de1fc1a6ad28667f041fbfa17d0359a13d0
Teri Charles§q! <tericharles@juno.com>
2015-03-31 13:18:07 -0600

    changed result_locator

--- pages/search.py
+++ pages/search.py
@@ -13,7 +13,7 @@ from pages.page import Page
 
 class Search(Base):
 
-    _result_locator = (By.CSS_SELECTOR, 'div.row > div.result')
+    _result_locator = (By.CSS_SELECTOR, 'div.row > div.rc')
     _search_button_locator = (By.CSS_SELECTOR, '.btn.primary:nth-of-type(1)')
     _advanced_options_button_locator = (By.CSS_SELECTOR, '.btn.primary:nth-of-type(2)')
     _advanced_options_locator = (By.CSS_SELECTOR, '.search-options')
